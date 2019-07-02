77c58d1e3c06e2454fd3065880382ac13e184ae0
schuelermine <nitrogenfan@gmail.com>
2019-07-02 21:34:30 +0200

    made shitty domain handling addition gonna revert

--- src/Data/Number/Internal.hs
+++ src/Data/Number/Internal.hs
@@ -38,46 +38,74 @@ data Specificity :: Type where
     Specific :: Specificity
     Unspecific :: Specificity
 
-data Constant :: Type where
-    Pi :: Constant
-    Phi :: Constant
-    E :: Constant
-    I :: Constant
+data Constant :: Domain -> Type where
+    Pi :: Constant R
+    Phi :: Constant R
+    E :: Constant R
+    I :: Constant C
 
 data Domain :: Type where
+    B :: Domain
     N :: Domain
     Z :: Domain
     Q :: Domain
     R :: Domain
     C :: Domain
 
-data Formula :: Specificity -> Natural -> Type where
-    Error :: Error -> Formula a n
-    Unknown :: Natural -> Formula a ('Succ n)
-    Constant :: Constant -> Formula a n
-    Value :: Complex Number -> Formula a n
-    Function :: Operand One -> Formula a n -> Formula a n
-    Operation :: Operand Two -> Formula a n -> Formula a n -> Formula a n
+data DomainHandling :: Natural -> Type where
+    Union :: DomainHandling Two
+    Simply :: Domain -> DomainHandling One
+    Negate :: DomainHandling One
+
+type family Union (a :: Domain) (b :: Domain) :: Domain
+type instance Union N x = x
+type instance Union x N = x
+type instance Union Z Q = Q
+type instance Union Z R = R
+type instance Union Q Z = Q
+type instance Union Q R = R
+type instance Union R Z = R
+type instance Union R Q = R
+type instance Union C x = C
+type instance Union x C = C
+
+type family Negate (a :: Domain) :: Domain
+
+type family HandleDomains (a :: DomainHandling Two) (b :: Domain) (c :: Domain) :: Domain
+type instance HandleDomains 'Union
+
+type family HandleDomain (a :: DomainHandling One) (b :: Domain) :: Domain
+
+data Formula :: Specificity -> Domain -> Natural -> Type where
+    Error :: Error -> Formula a b n
+    Unknown :: Natural -> Formula a b ('Succ n)
+    Constant :: Constant b -> Formula a b n
+    RealValue :: Complex Number -> Formula a 'R n
+    Unary :: Unary -> Formula a n -> Formula a n
+    Binary :: Binary -> Formula a n -> Formula a n -> Formula a n
     Fold :: Infinite Integer -> Infinite Integer -> Formula Unspecific ('Succ ('Succ n)) -> Formula Unspecific ('Succ n) -> Formula a n
+    Multiple :: [Formula a b n] -> Formula a b n
     -- for example: Fold (FromInteger '7) (FromInteger '10) (Operation Addition (Unknown one) (Unknown two)) (Operation Addition (Unknown one) (Unknown two))
 
-data Operand :: Natural -> Type where
-    Addition :: Operand Two
-    Negation :: Operand One
+data Operand :: DomainHandling Two -> Type where
+    Addition :: Operand Two 'Union
     Multiplication :: Operand Two
-    Reciprocation :: Operand One
-    Sine :: Operand One
-    Cosine :: Operand One
-    Tangent :: Operand One
-    Cotangent :: Operand One
-    Secant :: Operand One
-    Cosecant :: Operand One
-    Arcsine :: Operand One
-    Arccosine :: Operand One
-    Arctangent :: Operand One
-    Arccotangent :: Operand One
-    Arcsecant :: Operand One
-    Arccosecant :: Operand One
     Exponentiation :: Operand Two
     Logarithm :: Operand Two
     Root :: Operand Two
+
+data Unary :: DomainHandline One -> Type where
+    Negation :: Unary One
+    Reciprocation :: Unary One
+    Sine :: Unary One
+    Cosine :: Unary One
+    Tangent :: Unary One
+    Cotangent :: Unary One
+    Secant :: Unary One
+    Cosecant :: Unary One
+    Arcsine :: Unary One
+    Arccosine :: Unary One
+    Arctangent :: Unary One
+    Arccotangent :: Unary One
+    Arcsecant :: Unary One
+    Arccosecant :: Unary One
